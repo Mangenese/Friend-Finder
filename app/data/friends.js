9acdf3eb4a45c606f529cefceb26749549bba1a6
Mangenese <blakemangen20@gmail.com>
2020-03-06 16:17:41 -0700

    added the js data for the friends array

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// JS file that will hold our friend array
+var friends = [
+	{
+		name: 'Naruto',
+		photo: 'https://pmcvariety.files.wordpress.com/2015/07/naruto_movie-lionsgate.jpg?w=1000',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'1',
+			'5',
+			'5',
+			'2',
+			'1',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Luffy',
+		photo: 'https://i.pinimg.com/originals/0a/fb/46/0afb465b38987240997ed8d3cb054c64.png',
+		scores: [
+			'3',
+			'4',
+			'2',
+			'3',
+			'5',
+			'1',
+			'2',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Goku',
+		photo: 'https://static0.cbrimages.com/wordpress/wp-content/uploads/2019/11/Goku-Feature-Image.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Ichigo',
+		photo: 'https://i.pinimg.com/564x/63/8c/5c/638c5c69ecff8a285cdd57a2ada945cc.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jotaro',
+		photo: 'https://www.anime-planet.com/images/characters/jotaro-kujo-19368.jpg?t=1397848313',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Naofumi',
+		photo: 'https://i.pinimg.com/736x/70/61/41/706141d3e3d313caa1a40645b4cf9f48.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Edward',
+		photo: 'https://vignette.wikia.nocookie.net/fma/images/d/d3/Avatar_edward.png/revision/latest?cb=20180406214254',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Joker',
+		photo: 'https://www.candb.com/site/candb/images/artwork/Joker_Persona-5_Atlus_1920.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lightning',
+		photo: 'https://goombastomp.com/wp-content/uploads/2016/12/lightning_returns_final_fantasy_xiii-3368047.jpg',
+		scores: [
+			'5',
+			'3',
+			'2',
+			'4',
+			'1',
+			'5',
+			'3',
+			'3',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Chun Li',
+		photo: 'https://vignette.wikia.nocookie.net/projectxzone/images/2/23/Chun_Li.png/revision/latest/scale-to-width-down/340?cb=20170608025410',
+		scores: [
+			'1',
+			'5',
+			'4',
+			'1',
+			'3',
+			'1',
+			'2',
+			'4',
+			'6',
+			'1'
+		]
+	}
+];
+
+module.exports = friends;
