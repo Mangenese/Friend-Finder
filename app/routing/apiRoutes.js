cbe80a82f29ec157343ea6b7c70aa477003b74c2
Mangenese <blakemangen20@gmail.com>
2020-03-06 21:00:10 -0700

    added the pseudo code for the api route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,10 @@
+//require our dependencies 
+
 var path = require("path");
 
-var friends = require("../data/friends.js");
 
+var friends = require("../data/friends.js");
+//allow the file to be called in other files
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
@@ -11,23 +14,24 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res){
         console.log(req.body.scores);
 
-    // Receive user details (name, photo, scores)
+    
     var user = req.body;
 
-    // parseInt for scores
+    // for loop to grab the scores and make them ints
     for(var i = 0; i < user.scores.length; i++) {
       user.scores[i] = parseInt(user.scores[i]);
     }
 
-    // default friend match is the first friend but result will be whoever has the minimum difference in scores
+    
     var bestFriendIndex = 0;
+    //set a high number so there is nothing that can skew the data
     var minimumDifference = 40;
 
-    // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
-    //  whatever the difference is, add to the total difference
+    
     for(var i = 0; i < friends.length; i++) {
       var totalDifference = 0;
       for(var j = 0; j < friends[i].scores.length; j++) {
+          //this makes it so in case it goes negative it returns a positive integer
         var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
         totalDifference += difference;
       }
@@ -39,7 +43,7 @@ module.exports = function(app) {
       }
     }
 
-    // after finding match, add user to friend array
+   
     friends.push(user);
 
     // send back to browser the best friend match
